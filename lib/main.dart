2a578185a18e88998903940746953966e75b7177
ms747 <mayur.shah.ha@gmail.com>
2019-09-03 10:44:48 +0530

    new structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gymtrackerandroid/bloc/User.dart';
+import 'package:gymtrackerandroid/pages/Admin.dart';
 import 'package:gymtrackerandroid/pages/Login.dart';
-import 'package:provider/provider.dart';
-
-import 'components/Accordian.dart';
-
-void main() => runApp(LoginPage());
-
+ 
+void main() => runApp(MyApp());
+ 
 class MyApp extends StatelessWidget {
-  final _key = new GlobalKey<ScaffoldState>();
-
-  void _showBottomMenu() {
-    showModalBottomSheet(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        context: _key.currentContext,
-        builder: (_) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(10),
-                topRight: const Radius.circular(10),
-              ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(top: 10, bottom: 10),
-                        child: _buildtext(),
-                      ),
-                      Avatar(
-                        radius: 60,
-                      )
-                    ],
-                  ),
-                ),
-                Divider(
-                  color: Colors.black,
-                  indent: 30,
-                  endIndent: 30,
-                ),
-                ListTile(
-                  leading: Icon(Icons.lock),
-                  title: Text("Logout"),
-                  onTap: () {
-                    FirebaseAuth.instance.signOut();
-                    Future(() {
-                      Navigator.pushReplacement(_key.currentContext,
-                          MaterialPageRoute(builder: (ctx) => LoginPage()));
-                    });
-                  },
-                ),
-                Divider(
-                  color: Colors.black,
-                  indent: 30.0,
-                  endIndent: 30.0,
-                ),
-              ],
-            ),
-          );
-        });
-  }
-
-  Text _buildtext() {
-    return Text(
-      "Welcome, ${userBloc.getuser.displayName}",
-      style: Theme.of(_key.currentContext).textTheme.title,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Gym Tracker',
-      home: ChangeNotifierProvider.value(
-              child: Scaffold(
-          key: _key,
-          appBar: AppBar(
-            title: Text('Gym Tracker'),
-            actions: <Widget>[
-              FlatButton(
-                child: Avatar(
-                  radius: 20,
-                ),
-                onPressed: () {
-                  _showBottomMenu();
-                },
-              ),
-            ],
-          ),
-          body: Center(
-            child: Container(
-              child: _listView(),
-            ),
-          ),
-        ), value: userBloc,
-      ),
-    );
-  }
-}
-
-Widget _listView() {
-  return ListView(
-    primary: false,
-    children: <Widget>[
-      MyAccordian(),
-      MyAccordian(),
-    ],
-  );
-}
-
-class Avatar extends StatelessWidget {
-  final double radius;
-  Avatar({@required this.radius});
-
-  @override
-  Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: this.radius,
-      backgroundImage: NetworkImage(
-        userBloc.getuser.photoUrl,
-      ),
+      title: 'Material App',
+      initialRoute: '/',
+      routes: {
+        "/": (context) => LoginPage(),
+        "/admin" : (context) => AdminPage()
+      },
     );
   }
 }
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,93 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:gymtrackerandroid/bloc/User.dart';
-import 'package:gymtrackerandroid/main.dart';
-import 'package:provider/provider.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final _key = new GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    super.initState();
-    Future(() {
-      checkLoggedIn();
-    });
-  }
-
-  void checkLoggedIn() async {
-    var user = await FirebaseAuth.instance.currentUser();
-    final userState = Provider.of<UserBloc>(_key.currentContext);
-    if (user != null) {
-      userState.setloggedInStatus = true;
-      userState.setuser = user;
-      Navigator.of(_key.currentContext)
-          .push(MaterialPageRoute(builder: (ctx) => MyApp()));
-    } else {
-      userState.setloggedInStatus = false;
-    }
-  }
-
-  void login() async {
-    final _googleSignIn = GoogleSignIn();
-    final _auth = FirebaseAuth.instance;
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    var user = (await _auth.signInWithCredential(credential)).user;
-    checkLoggedIn();
-  }
+class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: ChangeNotifierProvider.value(
-        child: Scaffold(
-            key: _key,
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Gym Tracker",
-                    style: TextStyle(fontSize: 38),
-                  ),
-                  Text(
-                    "🏋",
-                    style: TextStyle(fontSize: 96),
-                  ),
-                  RaisedButton(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100)),
-                    onPressed: () {
-                      login();
-                    },
-                    color: Colors.red,
-                    child: Text(
-                      "Login with Google",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )
-                ],
-              ),
-            )),
-        value: userBloc,
-      ),
+    return Container(
+      child: Scaffold(
+        body: Text("Login Page"),
+      )
     );
   }
 }
